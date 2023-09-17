da73fb34d714bed2caa73d6876d47e98f6681d6f
Peter <szpeter@Yeah.net>
2023-09-18 03:09:22 +0800

    fluent vue project template

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <fluent-card>
+    <h2>{{msg}}</h2>
+    <fluent-button appearance="accent" v-on:click="onClick">Click Me</fluent-button>
+  </fluent-card>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: 'HelloWorld',
+    props: {
+      msg: String,
+    },
+    methods: {
+      onClick: function () {
+        console.log('clicked!');
+      },
+    },
+  };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
+import { provideFluentDesignSystem, fluentCard, fluentButton } from '@fluentui/web-components'
 import App from './App.vue'
 
+provideFluentDesignSystem().register(fluentCard(), fluentButton());
 createApp(App).mount('#app')
+console.log('finished');
